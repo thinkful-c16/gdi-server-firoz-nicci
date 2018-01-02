73169fb8395875061c8a46110775062a87be74d6
Nicci Williams <nicci@nicciwill.com>
2018-01-02 12:14:58 -0500

    Added courses seed data

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
+const data = require('./courses');
 
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
+app.get('/api/courses', (req, res) => {
+  res.json(data);
+});
+
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  dbConnect();
+  runServer();
 }
 
 module.exports = {app};
