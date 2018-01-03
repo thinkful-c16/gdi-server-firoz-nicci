1984e505e853643957219cd978938fd73207bbdb
Nicci Williams <nicci@nicciwill.com>
2018-01-03 16:11:31 -0500

    Connected to mLab

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/gdi-app',
+        process.env.DATABASE_URL || 'mongodb://gdi-admin:password@ds239557.mlab.com:39557/gdi-app',   //'mongodb://localhost/gdi-app'
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/gdi-app-test'
